bc5fc3cb08679a158cdfbc0639c22756396e0174
dinarydwihatami <dinarydwihatami01@yahoo.com>
2020-07-03 21:09:20 +0700

    upload kedua

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('tamplate.index')
+@section('content')
+    <div class="card-header">Dashboard</div>   
+
+    <h2>WELCOME TO MUSIC STAR</h2> 
+    <h3>Your Favorite Music Partner</h3>
+
+    
+    
+@endsection
